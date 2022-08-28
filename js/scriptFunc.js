35dcef6d4726ff0f3e1de13ad6447c2b5a311f39
Dypler <sun.zakup@mail.ru>
2022-08-28 19:18:53 +0400

    new commit

---
+++ js/scriptFunc.js
@@ -0,0 +1,104 @@
+"use strict"
+// let num = 20;
+
+// function showFirstMessege(text) {
+//     console.log ("text")
+//     let num = 10;
+//     console.log(num);
+// }
+// showFirstMessege("Hello World");
+// console.log(num);
+
+// // function calc(a,b) {
+// //     return (a + b);
+// // }
+// // console.log(calc(4, 5));
+// // console.log(calc(9, 5));
+// // console.log(calc(10, 5));
+
+// function ret() {
+//     let num = 50;
+//     //
+
+//     return num;
+// }
+// const anotherNum = ret();
+// console.log(anotherNum);
+
+// const logger = function () {
+//     console.log("Hello!");
+// };
+
+// logger();
+
+// const calc = (a, b) => a + b;
+
+// const usdCurr = 28;
+// const discount = 0.9;
+
+// function convert (amount, curr) {
+//     return curr * amount;
+
+//     // console.log(curr * amount);
+// }
+// function promotion (result) {
+//     console.log (result * discount);
+// }
+// const res = convert(500, usdCurr);
+// promotion(res);
+// convert(300, usdCurr);
+// convert(300, eurCurr);
+
+
+// function test() {
+//     for (let i = 0; i < 5; i++) {
+//         console.log(i);
+//         if (i === 3) return 
+//     }
+//     console.log('Done');
+// }
+// test();
+
+// function doNothing() {};
+// console.log(doNothing() === undefined);
+
+// function sayHello(name) {
+//     var phrase = "Привет, " + name + "!";
+//     return console.log(phrase);
+// }
+// sayHello('Антон');
+
+// function returnNeighboringNumbers() {
+    
+// }
+// function sayHello(name) {
+//     return `Привет, ${name}!`;
+// }
+
+// sayHello('Alex');
+
+// function returnNeighboringNumbers(num) {
+//     return [num - 1, num, num + 1];
+//     console.log(num);
+// }
+
+// returnNeighboringNumbers(5);
+
+const str = "teSt";
+const arr = [1,2,4];
+
+console.log(str.toUpperCase());
+console.log(str.toLowerCase());
+
+console.log(arr.length);
+const fruit = "Some fruit";
+console.log(fruit.indexOf("fruit"));
+const logg = "Hello world";
+console.log(logg.slice(6, 11));
+console.log(logg.substring(6, 11));
+
+const num = 12.2;
+console.log(Math.round(num));
+const test ="12.2px";
+console.log(parseInt(test));
+console.log(parseFloat(test));
